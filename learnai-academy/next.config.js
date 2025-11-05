7523eabe85d8cfc7cf6001b9d53fc5660afb0ad1
CodeCrafter-Hub <akawakwe@gmail.com>
2025-11-04 22:56:05 -0800

    Update next.config.js

--- learnai-academy/next.config.js
+++ learnai-academy/next.config.js
@@ -1,13 +1,20 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost'],
+    domains: ["localhost"],
   },
   experimental: {
     serverActions: true,
   },
-}
+  webpack: (config) => {
+    // Add alias support for "@/..."
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
